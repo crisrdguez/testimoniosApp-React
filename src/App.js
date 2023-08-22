1ff42125e749f0d3a01fe40ca0ec479db4554827
crisRR <140411058+crisrdguez@users.noreply.github.com>
2023-08-22 19:01:40 +0200

    initial commit componente basico para gestionar testimonios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import Testimonio from './components/Testimonio';
+
+
 
 function App() {
+  const testimonios = [
+    {
+      nombre:'Nagasaki',
+      pais:'Japón',
+      imagen:'1',
+      tituloTestimonio:'Oh My GOD',
+      testimonio:'Es un sitio muy bonito al que volvere porque hay muchas cosas bonitas, como sus Jardines: Isla y Principe. Como yo, que soy un Príncipe.¡Viva Aranjuez y su gente!'
+    },
+    {
+      nombre:'Miki',
+      pais:'Singapore',
+      imagen:'2',
+      tituloTestimonio:'Aligator',
+      testimonio:'Aranjuez me ha gustado porque tiene patos a los que puedes dar de comer, ademas en el río hay tortuguitas'
+    },
+    {
+      nombre:'Nagasaki',
+      pais:'Korea',
+      imagen:'3',
+      tituloTestimonio:'Ughuuuu',
+      testimonio:'Adoro el deporte al aire libre y no hay mejor lugar que Aranjuez para practicarlos'
+    },
+    {
+      nombre:'Punch',
+      pais:'China',
+      imagen:'4',
+      tituloTestimonio:'PUM PUM',
+      testimonio:'Me encanta comer, desde gyozas hasta ramen pasando por pizzas y perritos. Aranjuez está lleno de restaurantes en los que puedo llenar mi Panza'
+    }
+    
+  ];
+  
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor-principal'>
+        <h1>Testimonios de personas reales acerca del pueblo más bonito del mundo: Aranjuez</h1>
+        {testimonios.map((testimonio, index) => (
+          <Testimonio 
+          key={index}
+          nombre={testimonio.nombre}
+          pais={testimonio.pais}
+          imagen={testimonio.imagen}
+          tituloTestimonio={testimonio.tituloTestimonio}
+          testimonio={testimonio.testimonio}
+          />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/Testimonio.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import '../styles/Testimonio.css'
+
+function Testimonio(props){
+  //Quiero que si aparece la palabra Aranjuez salga en negrita
+  const formatText = (text, targetWords) => {
+    const words = text.split(' ');
+
+    return words.map((word, index) => {
+      if (targetWords.includes(word)) {
+        return (
+          <strong key={index}>
+            {word + ' '}
+          </strong>
+        );
+      }else{
+        return word + ' ';
+      }
+      
+    });
+  };
+
+  return(
+    //devuelve un componente jsx. contenedor-testimonio: el contenedor blanco exterior
+    <div className='contenedor-testimonio'> 
+      <img 
+        className='imagen-testimonio' 
+        src={require(`../img/persona-${props.imagen}.png`)}
+        alt='Foto de la persona' />
+      
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'>{props.nombre} de {props.pais}</p>
+        <p className='titulo-testimonio'><strong>{props.tituloTestimonio}</strong></p>
+        <p className='texto-testimonio'>"{formatText(props.testimonio, ['Aranjuez','aranjuez'])}"</p>
+      </div>
+        
+    </div>
+  );    
+}
+
+export default Testimonio;
